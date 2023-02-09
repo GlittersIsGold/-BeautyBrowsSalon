b1d2c01a574e5a23bcec6b6d1ca193b7283f0c19
student student <student@mgok.local>
2023-02-09 14:50:11 +0300

    Все данные импортированы, бэкап бд в скрипте sql

---
+++ View/Screens/General/PageWelcome.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeautyBrows.View.Screens.General
+{
+    /// <summary>
+    /// Логика взаимодействия для PageWelcome.xaml
+    /// </summary>
+    public partial class PageWelcome : Page
+    {
+        public PageWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdmin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
