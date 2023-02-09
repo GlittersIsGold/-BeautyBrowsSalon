4680612a99c08e4b1893a1ae4d96cd4a8df8e92b
student student <student@mgok.local>
2023-02-09 16:00:36 +0300

    таблица для администратора

--- View/Screens/Admin/PageAdministration.xaml.cs
+++ View/Screens/Admin/PageAdministration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BeautyBrows.Controller.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,17 @@ namespace BeautyBrows.View.Screens.Admin
         public PageAdministration()
         {
             InitializeComponent();
+            DataGridOfferService.ItemsSource = Connetction.BrowsEntities.Services.ToList();
+        }
+
+        private void BtnEditService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteService_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
