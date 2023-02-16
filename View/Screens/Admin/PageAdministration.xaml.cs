a770232ccfb3463ad88126c1fb35bdc1eaacba1c
student student <student@mgok.local>
2023-02-16 13:57:58 +0300

    combo

--- View/Screens/Admin/PageAdministration.xaml.cs
+++ View/Screens/Admin/PageAdministration.xaml.cs
@@ -24,7 +24,16 @@ namespace BeautyBrows.View.Screens.Admin
         public PageAdministration()
         {
             InitializeComponent();
-            DataGridOfferService.ItemsSource = Connetction.BrowsEntities.Services.ToList();
+
+            var ServiceData = Connetction.BrowsEntities.Services.ToList();
+
+            DataGridOfferService.ItemsSource = ServiceData;
+
+            CmbServiceList.DisplayMemberPath = "Title";
+            CmbServiceList.SelectedValuePath = "ID";
+            CmbServiceList.ItemsSource = ServiceData;
+
+            RunRecordsFound.Text = Convert.ToString(ServiceData.Count());
         }
 
         private void BtnEditService_Click(object sender, RoutedEventArgs e)
@@ -35,6 +44,32 @@ namespace BeautyBrows.View.Screens.Admin
         private void BtnDeleteService_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void TbSearchQuery_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string SearchQuery = Convert.ToString(TbSearchQuery.Text);
+
+            var FoundRespone = Connetction.BrowsEntities.Services
+                                                        .Where(x => x.Title.Contains(SearchQuery))
+                                                        .ToList();
+
+            DataGridOfferService.ItemsSource = FoundRespone;
+
+            RunRecordsFound.Text = Convert.ToString(FoundRespone.Count());
+        }
+
+        private void CmbServiceList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int SelectedServiceId = Convert.ToInt32(CmbServiceList.SelectedValue);
+
+            var FoundServices = Connetction.BrowsEntities.Services
+                                                         .Where(x => x.ID == SelectedServiceId)
+                                                         .ToList();
+
+            DataGridOfferService.ItemsSource = FoundServices;
+
+            RunRecordsFound.Text = Convert.ToString(FoundServices.Count());
         }
     }
 }
--- View/Screens/Guest/PageOfferService.xaml.cs
+++ View/Screens/Guest/PageOfferService.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using BeautyBrows.Controller.DataAccess;
-using BeautyBrows.Model.ADO;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BeautyBrows.View.Screens.User
 {
@@ -26,12 +14,46 @@ namespace BeautyBrows.View.Screens.User
         {
             InitializeComponent();
 
-            DataGridOfferService.ItemsSource = Connetction.BrowsEntities.Services.ToList();
+            var ServiceData = Connetction.BrowsEntities.Services.ToList();
+
+            DataGridOfferService.ItemsSource = ServiceData;
+
+            CmbServiceList.DisplayMemberPath = "Title";
+            CmbServiceList.SelectedValuePath = "ID";
+            CmbServiceList.ItemsSource = ServiceData;
+
+            RunRecordsFound.Text = Convert.ToString(ServiceData.Count());
         }
 
-        private void BtnOfferService_Click(object sender, RoutedEventArgs e)
+        private void BtnOfferService_Click(object sender, System.Windows.RoutedEventArgs e)
         {
 
+        }
+
+        private void TbSearchQuery_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string SearchQuery = Convert.ToString(TbSearchQuery.Text);
+
+            var FoundRespone = Connetction.BrowsEntities.Services
+                                                        .Where(x => x.Title.Contains(SearchQuery))
+                                                        .ToList();
+
+            DataGridOfferService.ItemsSource = FoundRespone;
+
+            RunRecordsFound.Text = Convert.ToString(FoundRespone.Count());
+        }
+
+        private void CmbServiceList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int SelectedServiceId = Convert.ToInt32(CmbServiceList.SelectedValue);
+
+            var FoundServices = Connetction.BrowsEntities.Services
+                                                         .Where( x => x.ID == SelectedServiceId)
+                                                         .ToList();
+
+            DataGridOfferService.ItemsSource = FoundServices;
+
+            RunRecordsFound.Text = Convert.ToString(FoundServices.Count());
         }
     }
 }
